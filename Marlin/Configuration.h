19c2c77055c125ad4d2a586e7d03b9f97f839c56
Mark <github@cnrdesigns.com>
2022-04-10 02:54:01 -0500

    fix z probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -907,7 +907,7 @@
 #define X_MAX_ENDSTOP_INVERTING true // mas // set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING true // mas // set to true to invert the logic of the endstop.
 #define Z_MAX_ENDSTOP_INVERTING true // mas // set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING true // mas // set to true to invert the logic of the probe.
+#define Z_MIN_PROBE_ENDSTOP_INVERTING false // mas // set to true to invert the logic of the probe.
 
 /**
  * Stepper Drivers
